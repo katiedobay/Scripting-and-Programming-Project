49e3b4be44a7eecd0ccaaaa8e29efbafddfb8770
kdobay <kdobay@wgu.edu>
2026-07-04 07:48:03 -0400

    added successful main.py code

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
 from weather_data import WeatherData
 
-weather = WeatherData(
-    latitude=27.9659,
-    longitude=-82.4389,
-    month=2,
-    day=23,
-    year=2025
-)
 
-weather.get_temperature_data()
-weather.get_wind_data()
-weather.get_precipitation_data()
+def main():
+    """
+    creates object and fetches weather info.
+    """
 
-print("Temperature Statistics")
-print("----------------------")
-print("Average:", weather.avg_temperature)
-print("Minimum:", weather.min_temperature)
-print("Maximum:", weather.max_temperature)
+    weather = WeatherData(
+        latitude=27.9659,
+        longitude=-82.4389,
+        month=2,
+        day=23,
+        year=2025
+    )
 
-print("\nWind Statistics")
-print("----------------")
-print("Average:", weather.avg_wind_speed)
-print("Minimum:", weather.min_wind_speed)
-print("Maximum:", weather.max_wind_speed)
+    # fetch weather data
+    weather.get_temperature_data()
+    weather.get_wind_data()
+    weather.get_precipitation_data()
 
-print("\nPrecipitation Statistics")
-print("-------------------------")
-print("Total:", weather.sum_precipitation)
-print("Minimum:", weather.min_precipitation)
-print("Maximum:", weather.max_precipitation)
+    # fetch temperature data
+    print("TEMPERATURE STATISTICS")
+    print("----------------------")
+    print(f"Average Temperature: {weather.avg_temperature:.2f}°F")
+    print(f"Minimum Temperature: {weather.min_temperature:.2f}°F")
+    print(f"Maximum Temperature: {weather.max_temperature:.2f}°F")
+
+    # fetch wind data
+    print("\nWIND STATISTICS")
+    print("----------------")
+    print(f"Average Wind Speed: {weather.avg_wind_speed:.2f} mph")
+    print(f"Minimum Wind Speed: {weather.min_wind_speed:.2f} mph")
+    print(f"Maximum Wind Speed: {weather.max_wind_speed:.2f} mph")
+
+    # fetch precipitation data
+    print("\nPRECIPITATION STATISTICS")
+    print("------------------------")
+    print(f"Total Precipitation: {weather.sum_precipitation:.2f} in")
+    print(f"Minimum Precipitation: {weather.min_precipitation:.2f} in")
+    print(f"Maximum Precipitation: {weather.max_precipitation:.2f} in")
+
+
+if __name__ == "__main__":
+    main()
